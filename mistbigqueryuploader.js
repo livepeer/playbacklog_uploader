636ae6098724724eec168a35a1b45fa769d5dcfd
Thulinma <jaron@vietors.com>
2023-04-20 18:46:18 +0200

    Added support for commandline options + integration with "livepeer-" style "connectors" in MistServer

--- mistbigqueryuploader.js
+++ mistbigqueryuploader.js
@@ -1,5 +1,57 @@
 #!/bin/node
 
+const crypto = require('crypto');
+
+var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+if (argv.j){
+  let capa = {
+    desc: "Handles uploading playback logs to BigQuery",
+    friendly: "BigQuery playback log uploader",
+    name: "livepeer-mist-bigquery-uploader",
+    required: {
+      "port": {
+        help: "Port to listen on with UDP socket",
+        name: "Listen port",
+        option: "--port",
+        type: "str"
+      },
+      "addr": {
+        help: "Address to listen on with UDP socket",
+        name: "Listen address",
+        option: "--addr",
+        type: "str"
+      },
+      "dataset": {
+        help: "BigQuery dataset name",
+        name: "BQ Dataset",
+        option: "--dataset",
+        type: "str"
+      },
+      "table": {
+        help: "BigQuery table name",
+        name: "BQ Table",
+        option: "--table",
+        type: "str"
+      }
+    },
+    optional: {
+      "gauth": {
+        help: "Google application authentication credentials JSON file path (sets GOOGLE_APPLICATION_CREDENTIALS)",
+        name: "Google App Auth credential JSON file path",
+        option: "--gauth",
+        type: "str"
+      }
+    }
+  };
+  process.stdout.write(JSON.stringify(capa) + "\n");
+  return 0;
+}
+if (argv.port){process.env.PORT = argv.port;}
+if (argv.addr){process.env.ADDR = argv.addr;}
+if (argv.dataset){process.env.DATASET = argv.dataset;}
+if (argv.table){process.env.TABLE = argv.table;}
+if (argv.gauth){process.env.GOOGLE_APPLICATION_CREDENTIALS = argv.gauth;}
+
 // Check environment
 if (!process.env.PORT){throw new Error("PORT environment variable not set!");}
 if (!process.env.ADDR){throw new Error("ADDR environment variable not set!");}
@@ -38,9 +90,13 @@ s.on('message', (msg, rinfo) => {
       "device": ua.device
     };
   }
-  console.log(json);
-  bq.dataset(process.env.DATASET).table(process.env.TABLE).insert([json]).catch((e) => {
-    console.log(`${e}`);
+  bq.dataset(process.env.DATASET).table(process.env.TABLE).insert([{log: JSON.stringify(json), timestamp: Date.now(), id: crypto.randomUUID()}]).catch((e) => {
+    console.warn(`${e}`);
+    if (e.errors){
+      for (err in e.errors){
+        console.warn(e.errors[err]);
+      }
+    }
   });
 });
 s.bind(process.env.PORT, process.env.ADDR);
