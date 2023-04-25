bd7eb09dafefc95ec7d9d33d33e12e7f86a8cc83
Thulinma <jaron@vietors.com>
2023-04-25 02:03:13 +0200

    Move libs to top, add in "result" entry in "ua" object.

--- mistbigqueryuploader.js
+++ mistbigqueryuploader.js
@@ -2,7 +2,19 @@
 
 const crypto = require('crypto');
 
+// Reference to the useragent parser
+const uap = require('ua-parser-js');
+
+// BigQuery library
+const {BigQuery} = require('@google-cloud/bigquery');
+const bq = new BigQuery();
+
+// UDP socket lib
+const udp = require('dgram');
+
+// Parse commandline arguments
 var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+
 if (argv.j){
   let capa = {
     desc: "Handles uploading playback logs to BigQuery",
@@ -58,16 +70,6 @@ if (!process.env.ADDR){throw new Error("ADDR environment variable not set!");}
 if (!process.env.DATASET){throw new Error("DATASET environment variable not set!");}
 if (!process.env.TABLE){throw new Error("TABLE environment variable not set!");}
 
-// Reference to the useragent parser
-const uap = require('ua-parser-js');
-
-// BigQuery library
-const {BigQuery} = require('@google-cloud/bigquery');
-const bq = new BigQuery();
-
-// UDP socket lib
-const udp = require('dgram');
-
 // Create listening UDP socket
 const s = udp.createSocket('udp4');
 s.on('listening', () => {
@@ -85,6 +87,7 @@ s.on('message', (msg, rinfo) => {
   if ("user_agent" in json){
     let ua = uap(json["user_agent"]);
     json["ua"] = {
+      "result": ua,
       "os": ua.os.name,
       "engine": ua.engine.name,
       "device": ua.device
